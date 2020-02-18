44f4790ba3681a7482dc3c2f6d7810dfc9d56d58
Yannick <yannick.boogaerts@technifutur.be>
2020-02-18 14:01:14 +0100

    réglage test

--- test/be/technifutur/myframework/MyFrameworkTest.java
+++ test/be/technifutur/myframework/MyFrameworkTest.java
@@ -3,6 +3,8 @@ package be.technifutur.myframework;
 import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Supplier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyFrameworkTest {
@@ -30,17 +32,20 @@ class MyFrameworkTest {
     public void testUneSaisie() {
         // Création du mock object
         Application app = EasyMock.createStrictMock(Application.class);
+        Supplier<String> in = EasyMock.createStrictMock(Supplier.class);
 
         // Définition du scénario d'utilisation du mock
         EasyMock.expect(app.display()).andReturn("affichage 1");
         EasyMock.expect(app.isFinish()).andReturn(false);
+        EasyMock.expect(in.get()).andReturn("fin");
         app.receive("fin");
         EasyMock.expect(app.display()).andReturn("affichage 1");
         EasyMock.expect(app.isFinish()).andReturn(true);
         // Préparation ed l'utilisation du mock
-        EasyMock.replay(app);
+        EasyMock.replay(app, in);
 
         MyFramework framework = new MyFramework();
+        framework.setIn(in);
         framework.start(app);
 
         // vérification que le scénario est terminé
